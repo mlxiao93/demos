87503c13641be2d87cc21ce296673312b1793a61
mlxiao <mlxiao93@163.com>
2017-02-21 12:12:05 +0800

    update

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,15 +1,1 @@
 import './index.scss'
-
-let box = document.querySelector('.box');
-
-let target = document.documentElement;
-// let target = box;
-console.log(target.clientHeight);
-console.log(target.offsetHeight);
-console.log(target.scrollHeight);
-
-console.log(box.getBoundingClientRect());
-
-// console.log(box.offsetHeight, box.clientHeight);
-// console.log(box.offsetTop);
-
